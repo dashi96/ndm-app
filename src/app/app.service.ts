766616d202404567193a696835792abef3c46e64
dancaranov.dashi <dancaranov.dashi@wb.ru>
2022-04-04 15:49:21 +0300

    fix

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
 
 export interface Route {
   uuid: string;
@@ -16,10 +15,6 @@ export class AppService {
   constructor(private http: HttpClient) { }
 
   getData(): Observable<Route[]> {
-    return this.http.get('./assets/routes.json').pipe(
-      map((response: any) => {
-        return response
-      })
-    );
+    return this.http.get<Route[]>('./assets/routes.json');
   }
 }
